3c252e68086018db09c98226407e5c0d8da22bc7
Gustavo9481 <guscode.apps@gmail.com>
2023-06-04 00:32:29 -0500

    Carro > creación de vistas / Tienda > botones: agregar al carro

--- ProyectoWeb/Carro/views.py
+++ ProyectoWeb/Carro/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .carro import Carro
+from Tienda.models import Producto
+from django.shortcuts import redirect
+
+
+# ------------------------------ BLOG
 
 # Create your views here.
+
+# Agregar
+# crea carro / obtiene id producto / agrega producto a carro / redirije a tienda
+def agregar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.agregar(producto=producto)
+    return redirect("tienda")
+
+
+# Eliminar
+# crea carro / obtiene id producto / elimina producto del carro / redirije a tienda
+def eliminar_producto(request, producto_id):    
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.eliminar(producto=producto)
+    return redirect("tienda")
+
+
+# Restar 
+# crea carro / obtiene id producto / resta producto a carro / redirije a tienda
+def restar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.restar_producto(producto=producto)
+    return redirect("tienda")
+
+
+# Limpiar
+# crea carro / limpiar carro / redirije a tienda
+def limpiar_carro(request, producto_id):
+    carro = Carro(request)
+    carro.limpiar_carro()
+    return redirect("tienda")
+
+
