d8c9e7653fada64a8398c1e35dd96db6c8e7d933
Gustavo9481 <guscode.apps@gmail.com>
2023-06-06 23:45:25 -0500

    Login > creación de formulario / Carro > mejoras de vista

--- ProyectoWeb/App_Autenticacion/urls.py
+++ ProyectoWeb/App_Autenticacion/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import VRegistro
+from .views import VRegistro, cerrar_sesion, loguear
 
 # ------------------------------ AUTENTICACIÓ# 
 
 urlpatterns = [
     path('', VRegistro.as_view(),name="Autenticacion"),
+    path('cerrar_sesion', cerrar_sesion, name="Cerrar_sesion"),
+    path('login', loguear, name="Login"),
 ]
 
 
--- ProyectoWeb/App_Autenticacion/views.py
+++ ProyectoWeb/App_Autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # ------------------------------ AUTENTICACIÓN
@@ -28,4 +28,32 @@ class VRegistro(View):
             return render(request, "registro/registro.html", {"form":form})
 
         
+    
+# cerrar sesión de usuario
+def cerrar_sesion(request):
+    logout(request)
+    return redirect("Home")
+
+# ingreso de usuario (login)
+def loguear(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            contra = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect("Home")
+            else:
+                messages.error(request, "Usuario no válido")
+        else:
+            messages.error(request, "Usuario no válido")
+
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form}) 
+
+
+
+
 
